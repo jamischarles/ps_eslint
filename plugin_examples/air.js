962b7269606390bfac7e6eaa7f5f999a3b088b06
Jamis Charles <jacharles@paypal.com>
2016-11-19 15:06:26 -0800

    END of Exploring ESLint Ecosystem Module

---
+++ plugin_examples/air.js
@@ -0,0 +1,9 @@
+// Let's get some apples
+
+function getApples() {
+  return ['Honeycrisp', 'Granny Smith'];
+}
+
+const flavors = getApples();
+console.log('flavors', flavors);
+
---
+++ plugin_examples/this.js
@@ -0,0 +1,11 @@
+
+var obj = {
+  color: 'red',
+  getColor: function() {
+    // return this.color;
+    return 'red';
+  }
+};
+
+obj.getColor();
+// -> 'red'
